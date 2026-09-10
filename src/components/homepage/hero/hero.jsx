00d8e38e2ac95a9dac4c695966001ae8ba690208
Fuzailnaeem <fuzailnaeem12@gmail.com>
2026-09-10 12:22:54 -0700

    Updated website changes

--- src/components/homepage/hero/hero.jsx
+++ src/components/homepage/hero/hero.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"; // Import Link
 import "./hero.css";
 import bannerImage from "../../../assets/ME_homepage_banner.webp";
 import StudyForm from "../studyform/Studysearchform.jsx";
-import TrustSection from '../../homepage/trustsection/TrustSection.jsx';
+import TrustSection from '../trustsection/TrustSection.jsx';
 
 const slides = [
   {
